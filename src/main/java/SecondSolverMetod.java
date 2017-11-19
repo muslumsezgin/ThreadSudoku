d07dcbc628be20f1953d083b3eb08c5ec5ce42fb
anil <anillcosar@gmail.com>
2017-11-19 18:19:06 +0300

    SecondSolver

--- src/main/java/SecondSolverMetod.java
+++ src/main/java/SecondSolverMetod.java
@@ -1,6 +1,54 @@
-public class SecondSolverMetod {
+public class SecondSolverMetod implements Runnable{
 
-    static boolean legal(int i, int j, int val, int[][] cells) {
+    int[][] sudoku;
+
+    MyBoolean myBoolean;
+
+
+
+    public SecondSolverMetod(int[][] sudoku, MyBoolean myBoolean) {
+        this.sudoku = sudoku;
+        this.myBoolean = myBoolean;
+
+    }
+
+    public void run() {
+        while (!myBoolean.isFinished()) {
+            if (solve(0, 0, sudoku)) {    // solves in place
+                System.out.println("Çözüldü");
+                myBoolean.setFinished(true);
+                return;
+            } else
+                System.out.println("none");
+        }
+    }
+
+     boolean solve(int i, int j, int[][] cells) {
+        if(!myBoolean.isFinished()) {
+            if (i == 9) {
+                i = 0;
+                if (++j == 9)
+                    return true;
+            }
+            if (cells[i][j] != 0)  // skip filled cells
+                return solve(i + 1, j, cells);
+
+            if (myBoolean.isFinished())
+                return false;
+
+            for (int val = 1; val <= 9; ++val) {
+                if (legal(i, j, val, cells)) {
+                    cells[i][j] = val;
+                    if (solve(i + 1, j, cells))
+                        return true;
+                }
+            }
+            cells[i][j] = 0; // reset on backtrack
+        }
+        return false;
+    }
+
+     boolean legal(int i, int j, int val, int[][] cells) {
         for (int k = 0; k < 9; ++k)  // row
             if (val == cells[k][j])
                 return false;
@@ -18,4 +66,6 @@ public class SecondSolverMetod {
 
         return true; // no violations, so it's legal
     }
+
+
 }
